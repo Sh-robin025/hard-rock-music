6b2f2367d20ab4327260c7f0d7ce6fc02d017747
Shahadat Robin <sh.robin025@gmail.com>
2021-02-09 17:58:21 +0600

    solve in h4 tag

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-
+// var input = document.getElementById("input").value;
 const getSongs = () => {
     const input = document.getElementById("input").value;
+    console.log(input);
     fetch(` https://api.lyrics.ovh/suggest/${input}`)
         .then(res => res.json())
-        .then(data => displaySong(data.data))
+        .then(data => displaySong(data.data,input))
         .catch(err => errMessage(err))
 }
 const errMessage = () => {
     document.getElementById("result").innerText = "";
     document.getElementById("error").innerText = "* input required .";
 }
-const displaySong = songs => {
+const displaySong = (songs,input) => {
     document.getElementById("error").innerText = "";
     const songContainer = document.getElementById("song-container");
     songContainer.innerHTML = '';
     // console.log(songs);
     if (songs.length === 0) {
-        document.getElementById("result").innerText = `There are no result for : ${input}.`
+        // const input = document.getElementById("input").value;
+        document.getElementById("result").innerText = `There are no result for : "${input}".`
     } else {
         songs.forEach(song => {
             document.getElementById("poster").innerHTML = "";
-            document.getElementById("result").innerText = `Search result for '${input}' :`
+            document.getElementById("result").innerText = `Search result for "${input}" :`
             const div = document.createElement("div");
             div.className = "single-result row align-items-center my-3 p-3";
             div.innerHTML = `
@@ -74,6 +76,7 @@ const goBack = () => {
     document.getElementById("content-area").style.display = "block";
 }
 
-const goHome = () => {
-    document.getElementById("content-area").style.display = "block";
-}
+// const goHome = () => {
+//     document.getElementById("poster").style.display = "block";
+//     document.getElementById("song-container").style.display = "none";
+// }
